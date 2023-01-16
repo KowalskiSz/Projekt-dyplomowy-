1d3830be242b8283f9c592320778e8810d676f0d
Szymon Kowalski <szymonkowalski.1998@o2.pl>
2023-01-16 17:17:50 +0100

    final commit

--- SinGen.py
+++ SinGen.py
@@ -54,43 +54,6 @@ class SinGen():
 
         return sineWave
 
-    def fft(self, sine): 
-
-        self.fStep = self.SampleRate / self.sampleSize
-        self.f = np.linspace(0, (self.sampleSize - 1) * self.fStep, self.sampleSize)
-
-        np_fft = np.fft.fft(sine)
-        #a = 2 / self.sampleSize * np.abs(np_fft)
-        a = np.abs(np_fft) / self.sampleSize
-
-        self.fplot = self.f[0:int(self.sampleSize/2 + 1)]
-        self.aPlot = 2 * a[0:int(self.sampleSize/2 + 1)]
-        self.aPlot[0] = self.aPlot[0] / 2
-
-        return a 
 
 
-# if __name__ == "__main__":
-
-#     sin = SinGen()
-#     sinus = sin.generateWave(1,50,2000)
-#     sin.fft(sinus)
-
-#     peak = find_peaks(sin.aPlot, height=0)
-#     heights = peak[1]['peak_heights']
-#     maxVals = list(map(float, heights))
-
-#     #newMaxVals = [x for x in maxVals]
-
-#     #print(type(sin.aPlot))
-    
-#     #print(maxVals)
-#     #print(len(heights))
-#     curAmp = np.amax(maxVals)
-#     print(curAmp)
-
-#     fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
-#     ax1.plot(sin.timeS, sinus, '.-')
-#     ax2.plot(sin.fplot, sin.aPlot, '.-')
-#     plt.show()
 
